3f3516d333c6448872278b728dd01654f7848462
Brian-L27 <22240802@life.hkbu.edu.hk>
2026-02-17 16:54:41 +0800

    fix the vue 3 environment problem and set up all the file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import UsersView from '../views/UsersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +9,20 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/users',
-      name: 'users',
-      component: UsersView
-    }
+
+
+{
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/LoginView.vue')
+},
+
   ]
 })
+
+
 
 export default router
